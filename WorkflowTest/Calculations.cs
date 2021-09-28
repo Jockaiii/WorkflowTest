0565593e4e7b3eaa9dddf1440c00d914b87c988d
00joaand <00jooand@gafe.molndal.se>
2021-09-28 10:12:40 +0200

    Add project files.

---
+++ WorkflowTest/Calculations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkflowTest
+{
+    public class Calculations
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ WorkflowTestTests/CalculationsTests.cs
@@ -0,0 +1,17 @@
+﻿using WorkflowTest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkflowTest.Tests
+{
+    [TestClass()]
+    public class CalculationsTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
